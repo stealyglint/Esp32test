211228b6c3f3690b6dc6942d5f6936f9d69ce49b
stealyglint <pdawson@PhysicsRT.cco>
2018-10-30 14:02:07 +0000

    Added chip_info capabilities..

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 
 uint64_t chipid;
+uint8_t coreNum;
+esp_chip_info_t chipInfo;
 int a=0;
 void setup() {
 	Serial.begin(115200);
@@ -10,10 +12,13 @@ void setup() {
 void loop() {
 
 	chipid=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
+
 	Serial.printf("ESP32 Chip ID = %04X",(uint16_t)(chipid>>32));//print High 2 bytes
 	Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
   Serial.printf("Hello World!\n");
   Serial.printf("%i\n",a);
+	esp_chip_info(&chipInfo);
+	Serial.printf("Num of Cores: %d\n", chipInfo.cores);
   a = a + 1;
 	delay(2000);
 
